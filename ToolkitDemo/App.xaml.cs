5ea3e914cdc535470563b157a1535d321c57254c
tetrodoxin <github@interlook.de>
2019-10-10 11:12:56 +0200

    - Added Style for TreeView - streamlined ComboBox- and ListView-Styles with selection colors of assembly

--- ToolkitDemo/App.xaml.cs
+++ ToolkitDemo/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace ToolkitDemo
 {
@@ -13,5 +14,10 @@ namespace ToolkitDemo
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+        }
     }
 }
--- ToolkitDemo/MainWindow.xaml.cs
+++ ToolkitDemo/MainWindow.xaml.cs
@@ -23,6 +23,32 @@ namespace ToolkitDemo
         {
             InitializeComponent();
             DataContext = this;
+            this.Activated += MainWindow_Activated;
+
+            var props = typeof(SystemColors).GetProperties(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.GetProperty)
+                .Where(p => p.PropertyType == typeof(Color))
+                .ToList();
+
+            var l = props.Select(p => p.GetValue(null, new object[0])).ToList();
+        }
+
+        private void MainWindow_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ListBox_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            var it = ((ListBox)sender).SelectedItem as ListBoxItem;
+            var bg = it.Background;
+            var t = bg;
+            var ad = AdornerLayer.GetAdornerLayer(it);
+            var ads = ad.GetAdorners(it);
+
         }
     }
 }
